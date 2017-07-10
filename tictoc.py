666cc3b8223e6962f41532fc9c636ff0b7d08bd3
Patrick Montalbano <pmontalbano@tesla.comgit config --global user.email pmontalbano@tesla.comgit config --global user.email pmontalbano@tesla.com>
2017-07-09 18:51:23 -0700

    Initial commit

---
+++ tictoc.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+from apscheduler.schedulers.blocking import BlockingScheduler
+import time, datetime
+
+print("Starting up!")
+
+sched = BlockingScheduler()
+
+try:
+    while True:
+        @sched.scheduled_job('cron', minute='1-55')
+        def water():
+            # Initialize GPIO
+            GPIO.cleanup()
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(18, GPIO.OUT)
+            GPIO.setup(21, GPIO.OUT)
+
+            # Actions
+            GPIO.output(21, GPIO.HIGH)
+            print('On @ ' + str(datetime.datetime.now()))
+            time.sleep(5)
+            GPIO.output(21, GPIO.LOW)
+            print('Off @ ' + str(datetime.datetime.now()))
+        sched.start()
+
+except KeyboardInterrupt:
+    sched.shutdown(wait=False)
+    print('Shutdown the Scheduler.')
